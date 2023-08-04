65cf9dabb203a1bbeb6a05d6f8ec11f29af2fa98
theGeekkit <sonofgondolin16@gmail.com>
2023-08-04 13:00:59 -0500

    8-4-23

--- src/app/weather-alert/weather-alert.component.ts
+++ src/app/weather-alert/weather-alert.component.ts
@@ -1,35 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { WeatherService } from './weather.service';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather-alert',
   templateUrl: './weather-alert.component.html',
   styleUrls: ['./weather-alert.component.css']
 })
-export class WeatherAlertComponent  {
+export class WeatherAlertComponent implements OnInit {
   latitude: number = 0;
   longitude: number = 0;
   weatherAlerts: any[] = [];
 
-  constructor(private http: HttpClient, private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService) { }
 
-  getWeatherAlerts(): void {
-    const apiUrl = 'https://localhost:5001/api/weather/weatheralerts'; // Replace with your API URL
-
-    const data = {
-      Latitude: this.latitude,
-      Longitude: this.longitude
-    };
-
-    this.http.post<any[]>(apiUrl, data).subscribe(
-      (response) => {
-        this.weatherAlerts = response;
-      },
-      (error) => {
-        console.error('Error fetching weather alerts:', error);
-      }
-    );
+  ngOnInit(): void {
+    this.getWeatherAlerts();
   }
 
+  getWeatherAlerts(): void {
+    this.weatherService.getWeatherAlerts(this.latitude, this.longitude)
+      .subscribe(
+        (response) => {
+          this.weatherAlerts = response;
+        },
+        (error) => {
+          console.error('Error fetching weather alerts:', error);
+        }
+      );
+  }
 }
