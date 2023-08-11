85ddfe8ba33d9d0bc75c4a606741dd1783e86025
theGeekkit <sonofgondolin16@gmail.com>
2023-08-11 12:13:10 -0500

    8-11-23 Co-authored-by: Joe Lambert <jLamwuzhere@users.noreply.github.com>

--- src/app/weather-alert/weather-alert.component.ts
+++ src/app/weather-alert/weather-alert.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../weather.service';
+import axios from 'axios';
 
 @Component({
   selector: 'app-weather-alert',
@@ -18,15 +19,32 @@ export class WeatherAlertComponent implements OnInit {
     this.getWeatherAlerts();
   }
 
-  getWeatherAlerts(): void {
-    this.weatherService.getWeatherAlerts(this.latitude, this.longitude)
-      .subscribe(
-        (response) => {
-          this.weatherAlerts = response;
-        },
-        (error) => {
-          console.error('Error fetching weather alerts:', error);
-        }
-      );
+  async getWeatherAlerts() {
+    // Make a GET request
+    try {
+      let response = await axios.get('https://api.weather.gov/alerts?point=37.01161240210997,-89.60530401388498');
+
+      let obj = response.data;
+      let features = obj.features;
+
+      // Filter the features
+      let topLevelFeatures = features.filter((obj: any) => obj.properties.references.length <= 5);
+
+      obj.features = topLevelFeatures;
+
+      // Write the data to a JSON file (note: this isn't suitable for Angular client-side apps)
+      // writeFileSync('fourth_update.json', JSON.stringify(obj));
+
+      console.log(topLevelFeatures.length);
+
+      console.log(response.data);
+      let data = response.data;
+      console.log(data.features.length);
+      data.features.forEach((feature: { id: any; }) => {
+        console.log(feature.id);
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
 }
