545d73c40e47a8bd25a56f65e380388b4d0e68ea
theGeekkit <sonofgondolin16@gmail.com>
2023-09-04 14:37:48 -0500

    9-4-23

---
+++ src/app/models.ts
@@ -0,0 +1,12 @@
+
+export interface Feature {
+  id: string;
+  properties: {
+    expires: string;
+    status: string;
+    messageType: string;
+    urgency: string;
+    severity: string;
+    references: { '@id': string }[];
+  };
+}
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,143 +1,105 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { interval, take } from 'rxjs';
+import * as fs from 'fs';
 
-
-import { HttpClient } from '@angular/common/http';
-import { Injectable, NgZone } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs'; // Import Observable
-import { interval, take, lastValueFrom } from 'rxjs';
+interface Feature {
+  id: string;
+  properties: {
+    expires: string;
+    status: string;
+    messageType: string;
+    urgency: string;
+    severity: string;
+    references: { '@id': string }[];
+  };
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
   alert: any = {};
- forecast: any = {};
- public $alertSubject = new BehaviorSubject<any>(null);
- private intervalId: any;
- private updateInterval: number = 60000;
- private displayWeather: any;
+  forecast: any = {};
+  public $alertSubject = new BehaviorSubject<any>(null);
+  private intervalId: any;
+  private updateInterval: number = 60000;
+  private displayWeather: any;
   private urgency: string = '';
   private event: string = '';
   private severity: string = '';
   private description: string = '';
+  private readyToSend: Feature[] = [];
 
- fileProgression = [
-  'initial_alerts.json',
-  'first_update.json',
-  'second_update.json',
-  'third_update.json',
-  'fourth_update.json',
- ];
+  fileProgression = [
+    'initial_alerts.json',
+    'first_update.json',
+    'second_update.json',
+    'third_update.json',
+    'fourth_update.json',
+  ];
 
   currentFileProgressPosition = 0;
 
- initialAlertId!: string;
+  initialAlertId!: string;
 
-
-  // Use Observable instead of BehaviorSubject
-
-  private alertSubject = new BehaviorSubject<any>(null);
-  public alert$: Observable<any> = this.alertSubject.asObservable(); // Expose as an Observable
-
-  constructor(private http: HttpClient, private ngZone: NgZone) {
-    this.displayWeather = this.getWeatherAlerts();
-    this.init().then(() => console.log('WeatherService, Initted'));
+  constructor() {
+    // Constructor logic here (if needed)
   }
 
- async init() {
-  console.log("display weather:", this.displayWeather)
-  this.intervalId = setInterval(() => {
-    this.ngZone.run(() => {
-      // this.getWeatherAlerts();
-      //  this.displayWeather = this.getWeatherAlerts();
-
-      this.urgency = this.displayWeather.features[0].properties.urgency;
-      this.event = this.displayWeather.features[0].properties.event;
-      this.severity = this.displayWeather.features[0].properties.severity;
-      this.description = this.displayWeather.features[0].properties.description;
-
-   });
-  }, this.updateInterval);
- }
-getEvent() {
-  console.log("event =" + this?.event);
-  return this?.event;
-}
-
- async getWeatherAlerts() {
-  let fileLookup = this.fileProgression[this.currentFileProgressPosition++];
-  var weatherBlob;
-  if (this.currentFileProgressPosition >= this.fileProgression.length) {
-   this.currentFileProgressPosition = 0;
-
-  }
-
-  try {
-    const result: any = this.http.get(`/assets/json/${fileLookup}`).pipe(take(2));
-    weatherBlob = (await lastValueFrom(result)) as any;
-    // console.log(weatherBlob.features);
-
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-  // console.log('function returned');
-  return weatherBlob;
-}
-
-
- async checkForAlertUpdates(alertFeatures: any[]) {
-  try {
-    let fileLookup = this.fileProgression[this.currentFileProgressPosition++];
-    if (this.currentFileProgressPosition >= this.fileProgression.length) {
-      this.currentFileProgressPosition = 0;
-      console.log("update", this.checkForAlertUpdates)
-    }
-
-    const result: any = await this.http.get(`/assets/json/${fileLookup}`).toPromise();
-    if (result && result.features?.length > 0) {
-      const updatedAlerts = result.features.filter((feature: any) => {
-        return (
-          feature.properties.references.includes(this.initialAlertId) &&
-          feature.properties.messageType === 'Update'
-        );
+  private async findReferencedIds(obj: { features: Feature[] }): Promise<string[]> {
+    const referencedIds: string[] = [];
+    obj.features.forEach((feature) => {
+      feature.properties.references.forEach((reference) => {
+        if (!referencedIds.includes(reference['@id'])) {
+          referencedIds.push(reference['@id']);
+        }
       });
+    });
+    return referencedIds;
+  }
 
-      if (updatedAlerts.length > 0) {
-        // Emit updated alerts through the Observable
-        this.alertSubject.next(updatedAlerts);
+  private async run() {
+    const dat = fs.readFileSync('first_update.json', 'utf8');
+    const obj: { features: Feature[] } = JSON.parse(dat);
+    const currentDate = new Date('2023-08-04T11:50:00-05:00');
+
+    const referencedIds = await this.findReferencedIds(obj);
+
+    const activeFeatures: Feature[] = [];
+    obj.features.forEach((feature) => {
+      const expirationDate = new Date(feature.properties.expires);
+      if (
+        !referencedIds.includes(feature.id) &&
+        feature.properties.status === 'Actual' &&
+        feature.properties.messageType !== 'Cancel' &&
+        expirationDate > currentDate &&
+        feature.properties.urgency === 'Immediate'
+      ) {
+        activeFeatures.push(feature);
       }
-    } else {
-      console.warn('Received invalid or unexpected data format:', result);
-    }
-  } catch (error) {
-    console.error('An error occurred while checking for updates:', error);
+      const readyToSendIds = new Set(this.readyToSend.map((feature) => feature.id));
+      const filteredSevereOrExtremeFeatures = severeOrExtremeFeatures.filter(
+        (feature) => !readyToSendIds.has(feature.id)
+      );
+    });
+
+    fs.writeFileSync('activeFeatures.json', JSON.stringify(activeFeatures)); // list of active alert for the end user
+
+    const severeOrExtremeFeatures = activeFeatures.filter(
+      (feature) =>
+        feature.properties.severity === 'Severe' ||
+        feature.properties.severity === 'Extreme'
+    );
+
+    const readyToSendIds = new Set(this.readyToSend.map((feature) => feature.id));
+    const filteredSevereOrExtremeFeatures = severeOrExtremeFeatures.filter(
+      (feature) => !readyToSendIds.has(feature.id)
+    );
+
+    fs.writeFileSync(
+      'readyToSend.json',
+      JSON.stringify(filteredSevereOrExtremeFeatures)
+    );
   }
-  async function execute() {
-    const source$ = interval(2000).pipe(take(5));
-    const finalNumber = await lastValueFrom(source$);
-    console.log(`The last data is ${finalNumber}`);
-  }
-  execute();
-}
-
-
-
-
-
-
-
-
- // getForecast() {
- //   // GET request
- //   // this.http.get('https://api.weather.gov/points/37.01161240210997,-89.60530401388498').subscribe((result: any) => {
- //     this.http.get('https://api.weather.gov/gridpoints/PAH/96,51/forecast').subscribe((result: any) => {
-//     // Check if 'data' exists and is not empty
- //     if (result.data && result.data.length > 0) {
-//       this.forecast = result.data[0];}
- //     //  else {
- //     //   // Handle the case when no data is available
- //     //   this.forecast = null;
- //     // }
-//   });
- // }
-
 }
