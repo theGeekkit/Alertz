a5f4e0ead109547d4e3ed3b8b72bdef04c8697e3
theGeekkit <sonofgondolin16@gmail.com>
2023-09-11 07:27:06 -0500

    updating http use

--- src/app/weather-alert/weather-alert.component.ts
+++ src/app/weather-alert/weather-alert.component.ts
@@ -1,30 +1,30 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../weather.service';
-import { Subscription, Observable } from 'rxjs'; // Import Subscription
+import { Subscription } from 'rxjs'; // Import Subscription
 
 @Component({
   selector: 'app-weather-alert',
   templateUrl: './weather-alert.component.html',
   styleUrls: ['./weather-alert.component.css']
 })
-export class WeatherAlertComponent implements OnInit, OnDestroy {
+export class WeatherAlertComponent implements OnInit {
   alert: any = {};
   forecast: any = {};
   private alertSubscription!: Subscription; // Declare Subscription
 
   constructor(public weatherService: WeatherService) { }
 
-  ngOnInit() {
-    this.alertSubscription = this.weatherService.alert.subscribe((updatedAlerts: any) => {
-      // Handle updated alerts here
-      this.alert = updatedAlerts;
-      // console.log('Received updated alerts:', updatedAlerts);
-    });
+  ngOnInit(): void {
+    this.fetchData();
   }
-
-  ngOnDestroy() {
-    // Unsubscribe to prevent memory leaks
-    this.alertSubscription.unsubscribe();
+  async fetchData() {
+    const jsonData = await this.weatherService.readyToSend();
+    if (jsonData) {
+      // Work with the JSON data here
+      console.log('Fetched JSON data:', jsonData);
+    } else {
+      // Handle the error
+      console.error('Failed to fetch JSON data.');
+    }
   }
 }
-
